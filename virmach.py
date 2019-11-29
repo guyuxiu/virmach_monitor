41f8e0d0af8e617fe8682582f832f26fb30b285b
guyuxiu <35220301+guyuxiu@users.noreply.github.com>
2019-11-29 20:01:15 +0800

    Update virmach.py

--- virmach.py
+++ virmach.py
@@ -3,17 +3,17 @@ import json
 import re
 import webbrowser
 import time
-
+#######此处设置配置
 Price = 10
 ram = 500
 cpu = 1
 location = "LOS ANGELES, CA"
-
+#######
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
 while True:
 	response = requests.get("https://billing.virmach.com/modules/addons/blackfriday/new_plan.json",params ="" , headers = headers)
 	res = response.json()
-	print(res) #内存，在下面多加一个判断即可
+	print(res) 
 	if float(re.search(r"\d+\.?\d*",res["price"]).group()) < Price:
 		if int(res["ram"]) >= ram:
 			if int(res["cpu"]) >= cpu:
